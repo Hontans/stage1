f2668148958848696eb80cba986a2d6a67239422
gullme <guillaumevar31@gmail.com>
2024-03-26 12:27:59 +0100

    🌐 (contact.php): Change language attribute from 'en' to 'fr' for better localization in French-speaking regions 💡 (contact.php): Add whitespace for better code readability and structure

--- contact.php
+++ contact.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
         <title>Stage macket</title>
+
         <link rel="stylesheet" href="style.css" />
     </head>
-
     <body>
         <header class="primary-header" data-visible>
             <div class="container">
